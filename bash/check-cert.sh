3864d9da669aabf0667e76d9a5d729aff902778f
ss027503 <srs6c7@gmail.com>
2023-05-03 10:49:07 -0500

    update cert check script

--- bash/check-cert.sh
+++ bash/check-cert.sh
@@ -1,5 +1,7 @@
 ## checks the certificate information of a remote site
 echo "what url do you want to cert check? example; msn.com"
-read SERVER_URL
+read -p "URL: " SERVER_URL
 
-echo | openssl s_client -showcerts -servername $SERVER_URL -connect $SERVER_URL:443 2>/dev/null | openssl x509 -inform pem -noout -text
+read -p "what port to use for https? [443]: " PORT
+port=${PORT:=443}
+echo | openssl s_client -showcerts -servername $SERVER_URL -connect $SERVER_URL:$PORT 2>/dev/null | openssl x509 -inform pem -noout -text
